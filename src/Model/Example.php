6b0f7549ffa63dad4868827386acd32a068f8cc2
Angelo Mesquita <angeloamesquita@gmail.com>
2025-04-30 11:13:48 -0300

    PHP OOP project skeleton ready

---
+++ src/Model/Example.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+final class Example
+{
+    public function sayHello(): string
+    {
+        return "Hi, OOP World in PHP!\n";
+    }
+}
